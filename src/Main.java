dcab17f4f8d8286f31122059a3ef8fb54a7d3fc5
elmirpro <elmir6384@outlook.com>
2025-10-25 15:52:40 +0400

    h

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    static int mybank(int text, int text1, int text2){
+        System.out.println("************");
+
+        return 2;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("1) Banka Xidmeti");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        switch (mybank(1,2,3)){
+            case 1:
+                System.out.println("222");
+                break;
+            case 2:
+                System.out.println("333");
+                break;
+        }
+
+
+
+
+    }
+}
