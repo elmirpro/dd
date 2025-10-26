f25a4efd7b549988b9d92ead2a05eafa843704c0
elmirpro <elmir6384@outlook.com>
2025-10-26 11:11:41 +0400

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
-    static int mybank(int text, int text1, int text2){
-        System.out.println("************");
-
-        return 2;
+    static int menu(){
+        System.out.println("***********");
+        System.out.print("""
+                1)Balans
+                2)Nağdlaşdırma
+                3)Balans Arttırma
+                4)Çıxış
+                5)Daxil Et:\s""");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        return a;
     }
 
 
     public static void main(String[] args) {
-        System.out.println("1) Banka Xidmeti");
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        switch (mybank(1,2,3)){
-            case 1:
-                System.out.println("222");
-                break;
-            case 2:
-                System.out.println("333");
-                break;
-        }
+        int result = menu();
+        System.out.println("***********************************************************************************************************************************************************");
+        System.out.println(result);
+
 
 
 
