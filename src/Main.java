83d7aa93606aac78c91d3133cf42860dabd8b169
elmirpro <elmir6384@outlook.com>
2025-10-26 12:08:27 +0400

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+
     static int menu() {
         System.out.println("***********");
         System.out.print("""
@@ -11,12 +12,18 @@ public class Main {
                 5)Daxil Et:\s""");
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-        return a;
 
+        return a;
     }
+
+    static void showBalance(double balans){
+        int allCoin = (int) (balans * 100);
+        int manat = allCoin / 100;
+        int coin = allCoin % 100;
+        System.out.println("Balanınız: " + manat + " manat, " + coin + " qəpik.");
+    }
+
     public static void main(String[] args) {
-        int result = menu();
-        System.out.println("***********************************************************************************************************************************************************");
-        System.out.println(result);
+        showBalance(0.24);
     }
 }
